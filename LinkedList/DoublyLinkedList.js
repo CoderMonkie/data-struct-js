3894443db11b68a67c9458c69e34521dadadf8c7
maonianyou <maonianyou@foxmail.com>
2019-12-03 18:01:50 +0800

    blog/data-struct-js

--- LinkedList/DoublyLinkedList.js
+++ LinkedList/DoublyLinkedList.js
@@ -2,21 +2,29 @@
  * 链表：双向链表
  */
 function DoublyLinkedList() {
+    // 记录链表首元素
     this.__head = null
+    // 记录链表尾元素
     this.__tail = null
+    // 记录链表元素个数
     this.__count = 0
-    
+
+    // 用Node表示链表内部元素
     function Node(data) {
         this.data = data
-        this.prev = null
-        this.next = null
+        this.prev = null // 指向前元素
+        this.next = null // 指向后元素
+
+        Node.prototype.toString = function () {
+            return this.data.toString()
+        }
     }
 
-    LinkedList.prototype.append = function(data) {
+    DoublyLinkedList.prototype.append = function (data) {
         var newNode = new Node(data)
 
         // 1.判断链表是否为空
-        if(this.__count === 0) {
+        if (this.__count === 0) {
             // 新元素既是首也是尾
             this.__head = newNode
             this.__tail = newNode
@@ -32,27 +40,27 @@ function DoublyLinkedList() {
         this.__count += 1
     }
 
-    LinkedList.prototype.insert = function(position, data) {
+    DoublyLinkedList.prototype.insert = function (position, data) {
         // 1.边界检查（插入位置）
-        if (position < 0 || positon > this.__count) return false
+        if (position < 0 || position > this.__count) return false
 
         var newNode = new Node(data)
 
         // 2.插入元素时链表为空
-        if(this.__count === 0) {
+        if (this.__count === 0) {
             this.__head = newNode
             this.__tail = newNode
         }
         // 3.链表非空
         else {
             // 3.1插入到链表头部
-            if(position === 0) {
+            if (position === 0) {
                 newNode.next = this.__head
                 this.__head.prev = newNode
                 this.__head = newNode
             }
             // 3.2插入到链表尾部
-            else if(possition === this.__count) {
+            else if (position === this.__count) {
                 this.__tail.next = newNode
                 newNode.prev = this.__tail
                 this.__tail = newNode
@@ -61,11 +69,11 @@ function DoublyLinkedList() {
             else {
                 var current = this.__head
                 var index = 0
-                while(index < position) {
+                while (index < position) {
                     current = current.next
                     index++
                 }
-                current.pre.next = newNode
+                current.prev.next = newNode
                 newNode.prev = current.prev
                 newNode.next = current
                 current.prev = newNode
@@ -74,42 +82,32 @@ function DoublyLinkedList() {
 
         // 4.计数加1
         this.__count += 1
-        
+
         return true
     }
 
-    LinkedList.prototype.remove = function(data) {
-        // 根据指定数据取得下标值
-        var index = this.indexOf(data)
-        // 检查下标值是否正常取到
-        if(index === -1) return null
-
-        // 调用 removeAt 方法进行删除
-        return this.removeAt(index)
-    }
-
-    LinkedList.prototype.removeAt= function (positon) {
+    DoublyLinkedList.prototype.removeAt = function (position) {
         // 1.边界检查
-        if(position <  0 || position >= this.__count) return null
-        
+        if (position < 0 || position >= this.__count) return null
+
         var current = this.__head
 
         // 2.只有一个元素
-        if(this.size() === 1){
+        if (this.size() === 1) {
             this.__head = null
             this.__tail = null
         }
         // 3.多个元素的情况
         else {
             // 3.1 删首
-            if(position === 0){
+            if (position === 0) {
                 current = this.__head
-        
+
                 this.__head = current.next
                 current.next.prev = null
             }
             // 3.2 删尾
-            else if(positon === this.__count - 1) {
+            else if (position === this.__count - 1) {
                 current = this.__tail
                 this.__tail = current.prev
                 this.__tail.next = null
@@ -118,7 +116,7 @@ function DoublyLinkedList() {
             else {
                 var index = 0
                 var current = this.__head
-                while(index < positon) {
+                while (index < position) {
                     current = current.next
                     index += 1
                 }
@@ -133,43 +131,57 @@ function DoublyLinkedList() {
         return current.data
     }
 
-    LinkedList.prototype.update = function(position, data) {
-        // 边界检查
-        if(position <  0 || position >= this.__count) return false
+    DoublyLinkedList.prototype.remove = function (data) {
+        // 根据指定数据取得下标值
+        var index = this.indexOf(data)
+
+        // 检查下标值是否正常取到
+        if (index === -1) return null
+
+        // 根据取到的下标，调用 removeAt 方法进行删除
+        return this.removeAt(index)
+    }
+
+    DoublyLinkedList.prototype.update = function (position, data) {
+        // 1.边界检查
+        if (position < 0 || position >= this.__count) return false
 
         var current = this.__head
         var index = 0
 
-        // 找到指定下标位置元素
-        while(index < position) {
+        // 2.找到指定下标位置元素
+        while (index < position) {
             current = current.next
         }
+
+        // 3.修改数据
         current.data = data
+
         return true
     }
 
-    LinkedList.prototype.getItem = function(position) {
-        // 边界检查
-        if(position <  0 || position >= this.__count) return
+    DoublyLinkedList.prototype.getItem = function (position) {
+        // 1.边界检查
+        if (position < 0 || position >= this.__count) return
 
         var current = this.__head
         var index = 0
 
-        // 找到指定下标位置元素
+        // 2.找到指定下标位置元素
         // => TODO：改善：根据position所在位置选择从Head还是从Tail开始查找
-        while(index < position) {
+        while (index++ < position) {
             current = current.next
         }
         return current.data
     }
 
-    LinkedList.prototype.indexOf = function(data) {
+    DoublyLinkedList.prototype.indexOf = function (data) {
         var current = this.__head
         var index = 0
 
         // 查找指定数据的节点
-        while(current) {
-            if(current.data == data) {
+        while (current) {
+            if (current.data == data) {
                 return index
             }
             current = current.next
@@ -179,24 +191,68 @@ function DoublyLinkedList() {
         return -1
     }
 
-    LinkedList.prototype.size = function() {
+    DoublyLinkedList.prototype.traverse = function (cb, reversal) {
+        if (!reversal) return this.forwardTraverse(cb)
+        return backwardTraverse(cb)
+    }
+
+    // Forward: Tail -> Head
+    DoublyLinkedList.prototype.forwardTraverse = function (cb) {
+        // TODO: cb 参数检查（回调函数）
+
+        var current = this.__tail
+        while (current) {
+            cb(current.data)
+            current = current.prev
+        }
+    }
+
+    // Backward: Head -> Tail
+    DoublyLinkedList.prototype.backwardTraverse = function (cb) {
+        // TODO: cb 参数检查（回调函数）
+
+        var current = this.__head
+
+        while (current) {
+            cb(current.data)
+            current = current.next
+        }
+    }
+
+    DoublyLinkedList.prototype.getHead = function () {
+        if (this.__head == null) return null
+        return this.__head.data
+    }
+
+    DoublyLinkedList.prototype.getTail = function () {
+        if (this.__tail == null) return null
+        return this.__tail.data
+    }
+
+    DoublyLinkedList.prototype.size = function () {
         return this.__count
     }
 
-    LinkedList.prototype.isEmpty = function() {
+    DoublyLinkedList.prototype.isEmpty = function () {
         return this.__count === 0
     }
 
-    LinkedList.prototype.toString = function() {
+    DoublyLinkedList.prototype.clear = function () {
+        this.__head = null
+        this.__tail = null
+        this.__count = 0
+    }
+
+    DoublyLinkedList.prototype.toString = function () {
         var str = '[HEAD]'
         var current = this.__head
-        while(current) {
+        while (current) {
             str += ' -> ' + current.data
             current = current.next
         }
-        str += str=='[HEAD]' 
-            ? ' -> Null <- [TAIL]'
-            : ' <- [TAIL]'
+        str += str == '[HEAD]' ?
+            ' -> Null <- [TAIL]' :
+            ' <- [TAIL]'
         return str
     }
 }
@@ -211,6 +267,19 @@ var dLst = new DoublyLinkedList()
 dLst.append('a')
 dLst.append('b')
 dLst.append('c')
-dLst.insert(0, '0')
-dLst.insert(5, '5')
-console.log(lst.toString())
+dLst.forwardTraverse(function (val) {
+    console.log('forward-traversing: ', val)
+})
+dLst.backwardTraverse(function (val) {
+    console.log('backward-traversing: ', val)
+})
+dLst.insert(0, 'Insert-Index=0')
+dLst.insert(3, 'Insert-Index=3')
+dLst.insert(5, 'Insert-Index=Count')
+console.log(dLst.toString())
+console.log('getItem(5) => ', dLst.getItem(5))
+console.log('remove("c") => ', dLst.remove('c'))
+console.log('removeAt(3) => ', dLst.removeAt(3))
+console.log('[removeAt(3)]--Result ↓ \r\n', dLst.toString())
+dLst.clear()
+console.log('After Clear : ', dLst.toString())
--- LinkedList/LinkedList.js
+++ LinkedList/LinkedList.js
@@ -2,47 +2,62 @@
  * 链表：单向链表
  */
 function LinkedList() {
+    // 记录链表首个元素
     this.__head = null
+    // 记录链表元素个数
     this.__count = 0
-    
+
+    // 用Node表示链表内部元素
     function Node(data) {
         this.data = data
         this.next = null
+
+        Node.prototype.toString = function () {
+            return this.data.toString()
+        }
     }
 
-    LinkedList.prototype.append = function(data) {
+    LinkedList.prototype.append = function (data) {
+        // 1.创建新元素
         var newNode = new Node(data)
 
-        if(this.__count === 0) {
+        // 2.1链表为空时，直接添加到末尾
+        if (this.__count === 0) {
             this.__head = newNode
-        } else {
+        }
+        // 2.2链表非空时，探查到末尾元素并添加新元素
+        else {
             var current = this.__head
-            while(current.next) {
+            while (current.next) {
                 current = current.next
             }
             current.next = newNode
         }
 
+        // 3.内部计数加1
         this.__count += 1
+
+        return true
     }
 
-    LinkedList.prototype.insert = function(position, data) {
-        // 边界检查（插入位置）
+    LinkedList.prototype.insert = function (position, data) {
+        // 1.边界检查（插入位置）
         if (position < 0 || position > this.__count) return false
 
+        // 2.创建新元素
         var newNode = new Node(data)
 
-        // 插入到链表头部
-        if(position === 0) {
+        // 3.1插入到链表头部
+        if (position === 0) {
             newNode.next = this.__head
             this.__head = newNode
         }
-        // 以外
+        // 3.2以外（包括插入到末尾）
         else {
             var previous = null
             var current = this.__head
             var index = 0
-            while(index < position) {
+            while (index < position) {
                 previous = current
                 current = current.next
                 index++
@@ -50,79 +65,102 @@ function LinkedList() {
             previous.next = newNode
             newNode.next = current
         }
+        // 4.内部计数加1
         this.__count += 1
+
         return true
     }
 
-    LinkedList.prototype.remove = function(data) {
+    LinkedList.prototype.remove = function (data) {
         var current = this.__head
         var previous = null
 
-        while(current) {
-            if(current.data == data) {
-                if(previous == null){
+        while (current) {
+            // 找到指定数据的元素，让当前元素不再被引用
+            if (current.data == data) {
+                if (previous == null) {
+                    // 没有前元素，要删除的是首元素，修改 Head 指针
                     this.__head = current.next
                 } else {
+                    // 修改前元素内部指针
                     previous.next = current.next
                 }
+                // 内部计数减1
                 this.__count -= 1
+                // 处理完成，返回 true
                 return true
             }
             previous = current
             current = current.next
         }
+        // 查找到最后没有找到指定数据的元素，返回 false
         return false
+
+        // 注：
+        // 也可以通过调用 indexOf 获取下标后再调用 removeAt 来实现
+        // 只是返回值会不同，看实际需要
     }
 
-    LinkedList.prototype.removeAt= function (position) {
-        // 边界检查
+    LinkedList.prototype.removeAt = function (position) {
+        // 1.边界检查
         if (position < 0 || position >= this.__count) return false
 
         var index = 0
         var previous = null
         var current = this.__head
-        while(index++ < position) {
+
+        // 2.找到指定位置元素
+        while (index++ < position) {
             previous = current
             current = current.next
         }
+        // 3.使当前元素不再被引用
         previous.next = current.next
-        current = null
+
+        // 4.内部计数减1
         this.__count -= 1
+
+        return current.data
     }
 
-    LinkedList.prototype.update = function(position, data) {
-        // 边界检查
-        if(position < 0 || position >= this.__count) return false
-        
+    LinkedList.prototype.update = function (position, data) {
+        // 1.边界检查
+        if (position < 0 || position >= this.__count) return false
+
         var current = this.__head
         var index = 0
 
-        while(index++ < position) {
+        // 2.找到指定位置元素
+        while (index++ < position) {
             current = current.next
         }
+        // 3.修改当前元素数据
         current.data = data
+
+        // 4.修改完成，返回 true
+        return true
     }
 
-    LinkedList.prototype.getItem = function(position) {
+    LinkedList.prototype.getItem = function (position) {
         // 边界检查
-        if(position < 0 || position >= this.__count) return
+        if (position < 0 || position >= this.__count) return
 
         var index = 0
         var current = this.__head
 
-        while(index < position) {
+        while (index < position) {
             current = current.next
             index += 1
         }
         return current.data
     }
 
-    LinkedList.prototype.indexOf = function(data) {
+    LinkedList.prototype.indexOf = function (data) {
         var current = this.__head
         var index = 0
 
-        while(current) {
-            if(current.data == data) {
+        while (current) {
+            if (current.data == data) {
                 return index
             }
             current = current.next
@@ -131,22 +169,27 @@ function LinkedList() {
         return -1
     }
 
-    LinkedList.prototype.size = function() {
+    LinkedList.prototype.size = function () {
         return this.__count
     }
 
-    LinkedList.prototype.isEmpty = function() {
+    LinkedList.prototype.isEmpty = function () {
         return this.__count === 0
     }
 
-    LinkedList.prototype.toString = function() {
+    LinkedList.prototype.clear = function () {
+        this.__head = null
+        this.__count = 0
+    }
+
+    LinkedList.prototype.toString = function () {
         var str = '[HEAD] -> '
         var current = this.__head
-        while(current) {
-            str += current.data + ' -> '
+        while (current) {
+            str += current.toString() + ' -> '
             current = current.next
         }
-        if(str === '[HEAD] -> '){
+        if (str === '[HEAD] -> ') {
             str = '[HEAD] -> Null'
         }
         return str
@@ -175,3 +218,7 @@ console.log(lst.toString(), 'remove-c')
 console.log('indexOf-b : ', lst.indexOf('b'))
 lst.update(3, 'b-updated')
 console.log('update-b : ', lst.toString())
+lst.removeAt(3)
+console.log('after removeAt(3) : ', lst.toString())
+lst.clear()
+console.log('after clear : ', lst.toString())
