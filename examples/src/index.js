2ac41b801f792275c40f6ef1069135d08d8ae911
Mao NianYou <maonianyou@gmail.com>
2019-12-19 17:44:50 +0800

    pakcage-coding

--- examples/src/index.js
+++ examples/src/index.js
@@ -1,22 +1,5 @@
-import Stack from '../../lib/Stack'
+import testStack from './stack.test'
+import testProirityQueue from './priorityqueue.test'
 
-var stack = new Stack()
-for (var i = 0; i < 5; i++) {
-    stack.push(i)
-}
-console.log('isEmpty: ', stack.isEmpty)
-console.log('size: ', stack.size)
-console.log(stack.toString())
-console.log(`pop: `, stack.pop())
-
-stack.traverse((ele,index)=>{
-    console.log(`Traversing-Stack:${index}: ${ele}`)
-})
-
-stack.traverse((ele,index)=>{
-    console.log(`Traversing-Stack:${index}: ${ele}`)
-}, true)
-
-console.log(`claer: `, stack.clear())
-console.log('isEmpty: ', stack.isEmpty)
-console.log('size: ', stack.size)
+testStack()
+testProirityQueue()
